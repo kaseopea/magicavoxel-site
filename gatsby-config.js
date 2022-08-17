4325373b01ea206eb295b86b0fb56a81b4e57750
Vitaly <kaseopea@gmail.com>
2022-08-17 20:21:07 +0300

    Initial Gatsby Setup

---
+++ gatsby-config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  siteMetadata: {
+    title: `MagicaVoxel by ephtracy`,
+    description: `Home of MagicaVoxel, MagicaCSG, Aerialod apps by ephtracy.`,
+    author: `kaseopea`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        sassOptions: {
+          precision: 6,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-bootstrap-5`,
+        short_name: `gb5-starter`,
+        start_url: `/`,
+        background_color: `#375a7f`,
+        theme_color: `#375a7f`,
+        display: `standalone`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-gatsby-cloud`,
+  ],
+}
